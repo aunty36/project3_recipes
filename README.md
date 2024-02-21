eb3dc13e0c453c1ca362c25ea0b27e6517254e17
Sharon Walker <sharonwalker36@gmail.com>
2024-02-21 18:52:24 -0500

    Initialized vite app

---
+++ README.md
@@ -0,0 +1,3 @@
+# Project3_recipes
+
+## By: Sharon Walker
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+const App = () => {
+  return (
+    <div>
+      <h1>My Recipe App</h1>
+    </div>
+  )
+}
+
+export default App
