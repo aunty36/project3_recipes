62d5c285024ab364597cf66ba2a5c52efedc69f0
Sharon Walker <sharonwalker36@gmail.com>
2024-02-27 17:22:50 -0500

    Began recipe layout deisgn.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
 import './App.css'
+import TopNav from './components/TopNav'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
-    <div>
-      <h1>My Recipe App</h1>
+    <div className="home">
+      <header>
+        <TopNav />
+      </header>
+      <main>
+        <h1>Easy Eats:</h1>
+        <h2>25 Winning Recipes from a tiny Toronto apartment test kitchen</h2>
+      </main>
+      <sidebar>
+        <h3>This is my sidebar</h3>
+      </sidebar>
+      <footer>
+        <Footer />
+      </footer>
     </div>
   )
 }
---
+++ src/components/TopNav.jsx
@@ -0,0 +1,9 @@
+const TopNav = () => {
+  return (
+    <div>
+      <h2>This is my top nav</h2>
+    </div>
+  )
+}
+
+export default TopNav
